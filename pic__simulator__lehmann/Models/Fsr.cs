ea9d9acafffbf6f8a8e79e8b5af03e0784d10bd2
Maximilian Burr <maximilian.burr@outlook.de>
2022-05-24 01:08:18 +0200

    Added indrect adressi g

--- pic__simulator__lehmann/Models/Fsr.cs
+++ pic__simulator__lehmann/Models/Fsr.cs
@@ -2,42 +2,10 @@
 {
     public class Fsr : Register
     {
-        private bool fsr_0 { get; set; }
-        private bool fsr_1 { get; set; }
-        private bool fsr_2 { get; set; }
-        private bool fsr_3 { get; set; }
-        private bool fsr_4 { get; set; }
-        private bool fsr_5 { get; set; }
-        private bool fsr_6 { get; set; }
-        private bool fsr_7 { get; set; }
 
-
-        public Fsr()
+        public Fsr() : base()
         {
-            fsr_0 = false;
-            fsr_1 = false;
-            fsr_2 = false;
-            fsr_3 = false;
-            fsr_4 = false;
-            fsr_5 = false;
-            fsr_6 = false;
-            fsr_7 = false;
-
+            //base.WriteBit(0,true);
         }
-
-        public override void Write(int value)
-        {
-            fsr_0 = true;
-        }
-
-        public override int Read()
-        {
-            int bits = 0;
-
-            //bits = bits << Convert.ToInt32();
-
-            return bits;
-        }
-
     }
 }
--- pic__simulator__lehmann/Models/IND.cs
+++ pic__simulator__lehmann/Models/IND.cs
@@ -2,16 +2,25 @@ namespace pic__simulator__lehmann.Models;
 
 public class IND : Register
 {
-    public Register FSR;
-    public Register[] speicher;
+    private Register _FSR;
+    private Datenspeicher _Speicher;
 
-    public IND(Register _fsr, Register[] _speicher)
+    public IND(Register _fsr, Datenspeicher _speicher)
     {
-        FSR = _fsr;
-        speicher = _speicher;
+        _FSR = _fsr;
+        _Speicher = _speicher;
     }
     public override int Read()
     {
-        return speicher[FSR.Read()].Read();
+        //Nötig um Zirkelauslesen zu vermeiden
+        if (_FSR.Read() == 0) return 0;
+        Console.WriteLine("indirect Read at {0}", _FSR.Read());
+        return _Speicher.At(_FSR.Read()).Read();
+    }
+
+    public override void Write(int value)
+    {
+        Console.WriteLine("indirect Write at {0}", _FSR.Read());
+        _Speicher.At(_FSR.Read()).Write(value);
     }
 }
