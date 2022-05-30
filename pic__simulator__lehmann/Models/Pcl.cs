2a488112ba721fa25f4396b5158aa169980cb286
Maximilian Burr <maximilian.burr@outlook.de>
2022-05-30 03:15:54 +0200

    Implemented PCL + PCLATH

--- pic__simulator__lehmann/Models/Pcl.cs
+++ pic__simulator__lehmann/Models/Pcl.cs
@@ -1,7 +1,24 @@
-﻿namespace pic__simulator__lehmann.Models
+﻿using System.Collections;
+
+namespace pic__simulator__lehmann.Models
 {
-    public class Pcl:Register
+    public class Pcl : Register
     {
-        
+        private Register _pclath;
+        public Pcl(Register pclath)
+        {
+            _pclath = pclath;
+            _inhalt = new BitArray(11);
+            _inhalt.SetAll(false);
+        }
+
+        public override void Write(int value)
+        {
+            int pcl = value & 2047;
+            _inhalt = new BitArray(new int[] {pcl});
+            int pclath = value & 14336;
+            pclath >>= 11;
+            _pclath.Write(pclath);
+        }
     }
 }
--- pic__simulator__lehmann/Models/Pclath.cs
+++ pic__simulator__lehmann/Models/Pclath.cs
@@ -1,24 +1,14 @@
-﻿namespace pic__simulator__lehmann.Models
+﻿using System.Collections;
+
+namespace pic__simulator__lehmann.Models
 {
     public class Pclath : Register
     {
-        private bool pclath_0 { get {return _inhalt[0]; }
-            set{}
+        public Pclath()
+        {
+               _inhalt = new BitArray(5);
+               _inhalt.SetAll(false);
+               } 
         }
-        private bool pclath_1 { get {return _inhalt[1]; }
-            set{}
-        }
-        private bool pclath_2 { get {return _inhalt[2]; }
-            set{}
-        }
-        private bool pclath_3 {
-            get { return _inhalt[3]; }
-            set{}
-        }
-        private bool pclath_4 { get {return _inhalt[4];}
-            set{}
-        }
-
-        //5-7 nicht implentiert
-    }
+    
 }
